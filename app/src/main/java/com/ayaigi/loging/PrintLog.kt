37d116227d4a5681b1bb3aa785bf5b3324cac977
ayaigi <86873009+ayaigi@users.noreply.github.com>
2022-02-05 21:46:43 +0100

    v0.2.0

--- app/src/main/java/com/ayaigi/loging/PrintLog.kt
+++ app/src/main/java/com/ayaigi/loging/PrintLog.kt
@@ -1,7 +1,5 @@
 package com.ayaigi.loging
 
-import kotlin.properties.Delegates
-
 
 enum class PrintLogIds(id: Int) {
     Fox(0),
@@ -16,14 +14,15 @@ class PrintLog {
     val Fox = Session()
     val Bee = Session()
     val Dog = Session()
+    val Cat = Session()
+    val Cow = Session()
 
     companion object {
         class InvalidLengthException(Is: Int, Should: Int) : Exception("is: $Is; should: $Should")
 
-        private const val del = ';'
-        private const val delS = del.toString()
+        private const val del = ";"
         private const val prec = 2
-        private fun Float.format(): String = "%.${prec}f".format(this)
+        private fun Float.format(precision: Int): String = "%.${precision}f".format(this)
     }
 
     inner class Session() {
@@ -35,17 +34,25 @@ class PrintLog {
             this.length = labels.size
             this.values.clear()
         }
-        fun add(l: List<Float>) {
-            if (l.size == length) {
-                values.add(l)
-            } else throw InvalidLengthException(l.size, length)
+        fun init(vararg labels: String){
+            init(labels.asList())
         }
-        fun print() {
+        fun add(items: List<Float>) {
+            if (items.size == length) {
+                values.add(items)
+            } else throw InvalidLengthException(items.size, length)
+        }
+        fun add(vararg items: Float) {
+            if (items.size == length) {
+                values.add(items.asList())
+            } else throw InvalidLengthException(items.size, length)
+        }
+        fun print(precision: Int = prec, separator: String = del) {
             println("----")
             println("Session:")
-            println(labels.joinToString(separator = delS))
+            println(labels.joinToString(separator = separator))
             for (v in values) {
-                println(v.joinToString(separator = delS) { it.format() })
+                println(v.joinToString(separator = separator) { it.format(precision) })
             }
             println("----")
         }
